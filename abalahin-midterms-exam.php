cd9a8b7e83e33b86ee57d8a804ac39023b691d1e
butin <cristine.abalahin@jmc.edu.ph>
2017-09-02 15:49:45 +0800

    add abalahin

---
+++ abalahin-midterms-exam.php
@@ -0,0 +1,8 @@
+<?php  
+	$colors = array('white', 'green', 'red', 'blue', 'black');  
+	echo "The memory of that scene for me is like a frame of film forever frozen at that moment:
+ the $color[4]  carpet, the $color[3] lawn, the $color[1] 
+        house, the leaden sky. The new president and his first lady. - Richard M. Nixon";  
+
+
+?>   
