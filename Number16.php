f82301d2e7bafced8299b35e4957e904fb805e53
jomelang <jomel.ang@jmc.edu.ph>
2017-09-02 16:28:53 +0800

    16

---
+++ Number16.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Midter Exam</title>
+	</head>
+	<body>
+		<?php
+			$color = array("white", "green", "red", "blue", "black");
+
+			echo "The memory of that scene for me is like a frame of film forever frozen at <br> 
+				  that moment: the " . $color[2] . " carpet, the " . $color[1] . " lawn, the " . $color[0] . " house, the leaden <br> 
+				  sky. The new president and his first lady. - Richard M. Nixon"
+
+
+		?>
+	</body>
+</html>
