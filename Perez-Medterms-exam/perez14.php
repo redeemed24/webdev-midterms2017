ae0b675e5c3460ebe800b02fcb269825d2df0834
Cristian Rey Perez <Cristian Rey Perez>
2017-09-02 15:54:20 -0700

    problem 14 humana maam

---
+++ Perez-Medterms-exam/perez14.php
@@ -0,0 +1,16 @@
+<?php
+
+$count = array (
+	"Christmas Island" => "Flying Fish Cove",
+	"Philippines" => "Manila",
+	"Indonesia" => "Jakarta",
+	"Israel" => "Jerusalem",
+	"Jordan" => "Amman"
+	);
+
+foreach($count as $country => $capital){
+	echo "$country - $capital <br>";
+
+}
+
+?>
