e20cebbd7ced92df8ec29c866e9beb17eb92826f
mrdale3 <dale.cartagena@jmc.edu.ph>
2017-09-02 15:35:09 +0800

    Midterm Exam

---
+++ cartagena_midtermExam/cartagena.php
@@ -0,0 +1,16 @@
+<?php
+
+$count = array (
+    "Indonesia" => "Jakarta",
+    "Thailand" => "bangkok",
+    "Japan" => "Tokyo",
+    "Malaysia" => "Kuala Lumpur",
+    "Taiwan" => "Taipei"
+    );
+
+foreach($count as $country => $capital){
+    echo "$country - $capital <br>";
+
+}
+
+?>
