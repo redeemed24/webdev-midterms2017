b55890865b1c7bee4ae76b6e184a6b8b83ee7669
EcelAlba <preceel.alba@jmc.edu.ph>
2017-09-02 15:43:47 +0800

    lacap kharmella

---
+++ lacapkharmella.php
@@ -0,0 +1,23 @@
+<?php
+$months = array(
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July ',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+);
+
+$i = 0;
+do
+{
+    echo $months[$i];
+    $i++;
+    echo "<br>";
+}while($i <12);
