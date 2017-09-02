94ad1b6132ca7dbe62419b04422f2842e650d4ec
hvillareal30 <huey.villareal@jmc.edu.ph>
2017-09-02 15:18:34 +0800

    Midterm Exam

---
+++ villareal-midterms/associative-array.php
@@ -0,0 +1,16 @@
+<?php
+
+$count = array (
+	"United States" => "Washington D.C.",
+	"Philippines" => "Manila",
+	"North Korea" => "Pyongyang",
+	"South Korea" => "Seoul",
+	"Japan" => "Tokyo"
+	);
+
+foreach($count as $country => $capital){
+	echo "$country - $capital <br>";
+
+}
+
+?>
